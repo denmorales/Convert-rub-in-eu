8d353cad10197e22b833e0794475f5db0e7458f1
Matveev <MatveevDU@mpei.ru>
2021-09-21 16:08:00 +0300

    Add project files.

---
+++ ConsoleApp71/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество рублей: ");
+        double rub = double.Parse(Console.ReadLine());
+        double cource = GetCourse();
+        Console.WriteLine("Курс евро: " + cource);
+        Console.WriteLine("{0} руб. = {1} евро.", rub, Math.Round(rub / GetCourse(), 2));
+        Console.ReadKey();
+    }
+    static double GetCourse()
+    {
+        WebClient client = new WebClient();
+        client.Encoding = System.Text.Encoding.UTF8;
+        string data = client.DownloadString("http://www.banki.ru/products/currency/cb/");
+        string ff = data.Substring(data.IndexOf("<strong>Евро</strong>"));
+        ff = ff.Substring(ff.IndexOf("<td>") + 4);
+        ff = ff.Remove(ff.IndexOf("</td>")).Replace('.', ',');
+        return double.Parse(ff);
+    }
+}
